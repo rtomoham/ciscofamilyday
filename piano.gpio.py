aa614f75f3ad0287918ea678316739ba472d3bc3
rtomoham <rtomoham@cisco.com>
2016-09-18 22:22:16 +0200

    Cleaned up the code

--- piano.gpio.py
+++ piano.gpio.py
@@ -19,40 +19,40 @@ pygame.mixer.init()
 # to use Raspberry Pi board pin numbers
 GPIO.setmode(GPIO.BOARD)
 
+NUMBER_OF_INPUT_CHANNELS = 4
+INPUT_CHANNEL = (18, 22, 24, 26)
+PITCH = ("do", "re", "mi", "fa")
+OUTPUT_CHANNEL = (12)
+
 # set up the GPIO channels - one input and one output
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(22, GPIO.OUT)
-GPIO.setup(24, GPIO.OUT)
-GPIO.setup(26, GPIO.OUT)
-
-GPIO.output(18, GPIO.LOW)
-GPIO.output(22, GPIO.LOW)
-GPIO.output(24, GPIO.LOW)
-GPIO.output(26, GPIO.LOW)
-
+for i in range(0, NUMBER_OF_INPUT_CHANNELS):
+        GPIO.setup(INPUT_CHANNEL[i], GPIO.OUT)
+        # Do we need this next line? I don't think so
+        GPIO.output(INPUT_CHANNEL[i], GPIO.LOW)
+        
 #Does it make sense to use Pull Up Pull DOwn???? 
-
 GPIO.setup(18, GPIO.IN)
 GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.setup(12, GPIO.OUT)
+GPIO.setup(OUTPUT_CHANNEL[0], GPIO.OUT)
+GPIO.output(OUTPUT_CHANNEL[0], GPIO.LOW)
 
 # output to pin 12
 GPIO.output(12, GPIO.LOW)
 #GPIO.output(12, GPIO.HIGH)
 
 # input from pin 18
-input_value = GPIO.input(18)
-input_value2 = GPIO.input(22)
-input_value3 = GPIO.input(24)
-input_value4 = GPIO.input(26)
+#input_value = GPIO.input(18)
+#input_value2 = GPIO.input(22)
+#input_value3 = GPIO.input(24)
+#input_value4 = GPIO.input(26)
 
 # Define a function per note -> Expect name( do, re mi, fa .. and INPUT pin)
-def note( Name, pin):
+def note( Name, channel):
 #	print "%s is pin %s" % (Name, pin)
-        print(Name + " is pin " + str(pin))
+        print(Name + " is channel " + channel + " which is pin " + str(CHANNEL[channel]))
         sound = pygame.mixer.Sound(Name + ".wav")
         #sound.play()
         while True:
@@ -60,10 +60,10 @@ def note( Name, pin):
                 #GPIO.wait_for_edge(pin, GPIO.RISING)
                 time_stamp = time.time()
                 time_now = time_stamp
-                while (time_now - time_stamp <0.0750) and (GPIO.input(pin)==1):
+                while (time_now - time_stamp <0.0750) and (GPIO.input(CHANNEL[channel])==1):
                         time_now = time.time()
                 #time.sleep(0.01)
-                if (GPIO.input(pin) == 1):
+                if (GPIO.input(CHANNEL[channel]) == 1):
                         #GPIO.output(12, GPIO.HIGH)
                         sound.play()
                         print(Name)
@@ -72,11 +72,9 @@ def note( Name, pin):
                         
 # Create threads per note and per sensor
 try:
-        thread.start_new_thread( note, ("do", 18, ) )
-        thread.start_new_thread( note, ("re", 22, ) )
-        thread.start_new_thread( note, ("mi", 24, ) )
-        thread.start_new_thread( note, ("fa", 26, ) )
-
+        for i in range(0, NUMBER_OF_INPUT_CHANNELS):
+                thread.start_new_thread( note, (PITCH[i], i, ) )
+                
 except:
         print("Error: unable to start thread")
 
